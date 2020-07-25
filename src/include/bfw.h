9e5d2c14f57bcd7f9eeb49415e67c94786d0bb57
Christian Deacon <gamemann@gflclan.com>
2020-07-25 16:12:38 +0000

    Bumped max filters to 55.

--- src/include/bfw.h
+++ src/include/bfw.h
@@ -4,7 +4,7 @@
 #include <inttypes.h>
 
 #define MAX_PCKT_LENGTH 65535
-#define MAX_FILTERS 50
+#define MAX_FILTERS 55
 #define MAX_TRACK_IPS 100000
 #define MAX_PAYLOAD_LENGTH 1500
 #define MAX_SEND_LENGTH 1024
